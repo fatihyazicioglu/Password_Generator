d45d80f043be0799786d8f2bb0230bf37ea22172
fatihyazicioglu <f.yzcgl1@gmail.com>
2022-05-31 16:19:40 +0200

    updated

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const form = document.getElementById('passwordGeneratorForm')
 const passwordDisplay = document.getElementById('passwordDisplay')
 
 
+function randomColor(){
+  let one = Math.floor(Math.random () * 256)
+  let two = Math.floor(Math.random () * 256)
+  let three = Math.floor(Math.random() * 256)
+  let a = Math.random()
+  return `rgba(${one},${two},${three},${a})`
+        }
+   passwordDisplay.style.backgroundColor = randomColor()
+
 
 const randomHex = () => {
   let result = [];
@@ -9,12 +18,21 @@ const randomHex = () => {
   for (let i = 0; i < 8; i++) {
     result.push(numAndStr[Math.floor(Math.random() * numAndStr.length)]);
   }
-  passwordDisplay.innerHTML = result.join("");
+  passwordDisplay.innerHTML = result.join(" - ");
   return result.join("");
 };
-for (const item of form) {
+
+
+   for (const item of form) {
   item.style.backgroundColor = `#${randomHex()}`;
 }
+
+
+
+
+
+
+
 
 // for (let i = 0; i < items.length; ) {
 //   items[i].style.backgroundColor = "gray";
